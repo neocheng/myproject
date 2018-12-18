4cd4df34f94698208e067551d5a5c066a2325be8
neo_nb <neo.cheng@tac.com.tw>
2018-12-18 21:19:36 +0800

    Initial commit

---
+++ src/main/java/com/trainingcode/Person.java
@@ -0,0 +1,9 @@
+package com.trainingcode;
+
+public class Person {
+
+    public void hello(){
+        System.out.println("Hello World Java");
+    }
+
+}
---
+++ src/main/kotlin/com/trainingcode/Hello.kt
@@ -0,0 +1,11 @@
+package com.trainingcode
+
+fun main(args: Array<String>) {
+//    print("Hello kotlin !!! ")
+}
+
+class Human{
+    fun hello(){
+        print("Hello kotlin !!! ")
+    }
+}
